1f2829d65799cbfaea36b6dbac8ed4c0d74bb3c5
andrewyancey <andrewyanceygamedev@gmail.com>
2026-03-14 23:19:32 -0400

    restored and rewrote files that were accidentally deleted in last commit index.html, index.js, readme.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# forecast-app
+A simple frontend app that pulls weather data from open-meteo
+
+# Instructions for contribution
+Please push all contributions to the `dev` branch rather than `main` so changes can be reviewed and tested before they are merged into the stable production branch.
+
+# Credits
+Weather icons provided by https://erikflowers.github.io/weather-icons/
---
+++ scripts/index.js
@@ -0,0 +1,87 @@
+import * as api from "./api.js";
+
+// used to decide which values to pull from the API, and which ID they are mapped to
+const currentWeatherOptions = {
+    temperature_2m: "temp",
+    relative_humidity_2m: "humidity",
+    precipitation: "precipitation",
+    surface_pressure: "pressure",
+    wind_speed_10m: "wind-speed",
+    wind_direction_10m: "wind-direction",
+    wind_gusts_10m: "wind-gusts",
+    weather_code: "weather-code"
+}
+
+async function formSubmit(event) {
+    event.preventDefault();
+
+    var zip = event.currentTarget.querySelector("#zip").value;
+    if (zip.length !== 5) {
+        alert(`you have entered an incompatible zip code: ${zip}`);
+    }
+    else {
+        var location = await api.getCoords(zip);
+        var weather = await api.getWeather(location, currentWeatherOptions);
+        showData(weather, location);
+    }
+}
+
+function iconFromCode(code) {
+    const weatherCodeIcons = {
+        0: "wi-day-sunny.svg",
+        1: "wi-day-sunny.svg",
+        2: "wi-day-sunny-overcast.svg",
+        3: "wi-cloud.svg",
+        45: "wi-day-fog.svg",
+        48: "wi-day-fog.svg",
+        51: "wi-day-showers.svg",
+        53: "wi-day-showers.svg",
+        55: "wi-day-rain.svg",
+        56: "wi-day-showers.svg",
+        57: "wi-day-rain.svg",
+        61: "wi-day-showers.svg",
+        63: "wi-day-rain.svg",
+        65: "wi-day-rain.svg",
+        66: "wi-day-showers.svg",
+        67: "wi-day-rain.svg",
+        71: "wi-day-snow.svg",
+        73: "wi-day-snow.svg",
+        75: "wi-day-snow.svg",
+        77: "wi-day-snow.svg",
+        80: "wi-day-showers.svg",
+        81: "wi-day-rain.svg",
+        82: "wi-day-rain.svg",
+        85: "wi-day-snow.svg",
+        86: "wi-day-snow.svg",
+        95: "wi-day-thunderstorm.svg",
+        96: "wi-day-thunderstorm.svg",
+        99: "wi-day-thunderstorm.svg"
+    };
+    const icon = weatherCodeIcons[code];
+    if (icon === undefined) throw new Error("code not found in weatherCodeIcons");
+    return icon;
+}
+
+function showData(weather, location) {
+    document.querySelector(".content-area").style.display = "flex";
+    fillData(weather);
+    fillData(location);
+    if (weather["weather-code"] !== undefined)
+    {
+        const iconLocations = "./assets/svg-icons/"
+        document.getElementById("weather-icon").src =  iconLocations + iconFromCode(weather["weather-code"]);
+    }
+}
+
+function fillData(data) {
+    Object.entries(data).forEach(([key, value]) => {
+        const el = document.getElementById(key);
+        if (el) el.textContent = value;
+    })
+}
+
+function addHandlers() {
+    document.getElementById("input-form").addEventListener("submit", formSubmit);
+}
+
+addHandlers();
